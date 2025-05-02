0e5db775d6e408c5057618faf5bb9104edcb4074
Lior Bas <look551@gmail.com>
2025-05-02 18:08:19 +0300

    added notify and finished UserService

---
+++ Frontend/src/Utils/AppConfig.ts
@@ -0,0 +1,6 @@
+class AppConfig {
+  public readonly register = "http://localhost:4000/api/register/";
+  public readonly login = "http://localhost:4000/api/login/";
+}
+
+export const appConfig = new AppConfig();
