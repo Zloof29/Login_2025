6d4eaf606b4812f41cf4fb75c3766722bfe3f20b
Lior Bas <look551@gmail.com>
2025-05-03 13:47:04 +0300

    added UserArea

---
+++ Frontend/src/Components/UserArea/Login/Login.tsx
@@ -0,0 +1,5 @@
+import styles from "./Login.module.css";
+
+export function Login(): React.ReactElement {
+  return <div className={styles.Login}></div>;
+}
---
+++ Frontend/src/Components/UserArea/UserMenu/UserMenu.tsx
@@ -0,0 +1,5 @@
+import styles from "./UserMenu.module.css";
+
+export function UserMenu(): React.ReactElement {
+  return <div className={styles.UserMenu}></div>;
+}
