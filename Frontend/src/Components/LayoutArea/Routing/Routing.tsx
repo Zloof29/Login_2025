39e8ba3385de3e7e0dd0957dc7969442acc2f46e
Lior Bas <look551@gmail.com>
2025-05-05 13:07:36 +0300

    fixed input

--- Frontend/src/Components/LayoutArea/Routing/Routing.tsx
+++ Frontend/src/Components/LayoutArea/Routing/Routing.tsx
@@ -3,6 +3,7 @@ import styles from "./Routing.module.css";
 import { Page404 } from "../Page404/Page404";
 import { Login } from "../../UserArea/Login/Login";
 import { Register } from "../../UserArea/Register/Register";
+import { UserPanel } from "../../ContentArea/UserPanel/UserPanel";
 
 export function Routing(): React.ReactElement {
   return (
@@ -10,6 +11,7 @@ export function Routing(): React.ReactElement {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/userPanel" element={<UserPanel />} />
         <Route path="*" element={<Page404 />} />
       </Routes>
     </div>
